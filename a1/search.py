400058a513c9241917655a4079ee8d80056e4eb2
Joe O'Neal <joeoneal641@gmail.com>
2025-08-30 14:15:41 -0600

    minimalized node class

--- a1/search.py
+++ a1/search.py
@@ -3,17 +3,10 @@ from collections import dequeu
 ## need node class to track state, pointer to parent, action, path cost, and depth
 class Node:
 
-    def __init__(self, state, parent=None, action=None, cost=0):
+    def __init__(self, state, parent=None):
         self.state = state
         self.parent = parent
-        self.action = action
-        self.cost = cost
-        self.depth = 0
-        if parent:
-            self.depth += parent.depth
         
     def __str__(self):
         return f'Node {self.state}'
     
-    def next_nodes(self):
-        pass
