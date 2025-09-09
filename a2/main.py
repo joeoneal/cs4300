c766336f95c1890b04376d560c95b7c88f41c424
Joe O'Neal <joeoneal641@gmail.com>
2025-09-09 11:17:10 -0600

    all works

--- a2/main.py
+++ a2/main.py
@@ -2,21 +2,24 @@ from search import aStar
 from domains.problem import Problem
 
 def main():
-    heuristics_to_test = [
-        Problem.heuristic0, 
-        Problem.heuristic1, 
-        Problem.heuristic2
-    ]
+
 
     for i in range(3):
-        h_func = heuristics_to_test[i]
+
         print(f'RUNNING WITH HEURISTIC{i}')
         totalne = 0
         totalng = 0
         totalfs = 0
         totalsd = 0
-        for i in range(30):
+        for _ in range(30):
             problem = Problem()
+            heuristics_to_test = [
+                problem.heuristic0, 
+                problem.heuristic1, 
+                problem.heuristic2
+            ]
+
+            h_func = heuristics_to_test[i]
             solution = aStar(problem, h_func)
             # print(f'\nnodes expanded: {solution[0]} ')
             # print(f'nodes generated: {solution[1]}')
@@ -31,10 +34,10 @@ def main():
         avg_fs = totalfs / 30
         avg_sd = totalsd / 30
 
-        print(f'\nAverage nodes expanded: {avg_ne} ')
-        print(f'Average number of nodes generated: {avg_ng}')
-        print(f'Average max frontier size: {avg_fs}')
-        print(f'Average solution depth/cost: {avg_sd}')
+        print(f'\nAverage nodes expanded: {round(avg_ne, 2)} ')
+        print(f'Average number of nodes generated: {round(avg_ng, 2)}')
+        print(f'Average max frontier size: {round(avg_fs, 2)}')
+        print(f'Average solution depth/cost: {round(avg_sd,2)}')
         print()
     
         
